f4d8ad4c6be8dd4429c5af702f09bdb39d8eb7b2
riyasvk <riyasvk>
2019-01-18 07:34:46 +0530

    scan

--- suspend.sh
+++ suspend.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 USER=$1
 TICKET=$2
+
+if [ "$USER" == " ]; then
+	echo "USER must be specified"
+	exit 1
+fi
+
+if [ "$TICKET" == " ]; then
+        echo "TICKET must be specified"
+        exit 1
+fi
+
 sudo /app/bin/suspension-backend.sh -u $USER -s addon -l hacked -e "Please refer ticket $TICKET or contact support." -m
-sudo /app/bin/suspension-backend.sh -u $USER -s http -l hacked -e "Please refer ticket $TICKET or contact support." -m
+sudo /app/bin/suspension-backend.sh -u $USER -s http -l hacked -e "Please refer ticket # $TICKET or contact support." -m
