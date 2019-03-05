86191065b743915aecce749539d403d82f56ac16
riyasvk <riyasvk>
2019-03-05 06:50:49 +0530

    commit

---
+++ scan2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USERNAME=$1
+
+if [ "$1" == "" ]; then
+        echo "ERROR: domain name must be specified"
+        exit 1
+fi
+
+host_name=$(hostname  |cut -d. -f 1)
+echo "Hostname = $host_name"
+
+EMAIL=$(sudo cat /var/cpanel/users/$USERNAME |grep CONTACTEMAIL= |cut -d= -f 2)
+echo "EMAIL = $EMAIL"
+OWNER=$(sudo cat /var/cpanel/users/$USERNAME |grep OWNER= |cut -d= -f 2)
+echo "OWNER = $OWNER"
+if [ "$OWNER" != "root" ]; then
+  OWNER_EMAIL=$(sudo cat /var/cpanel/users/$OWNER |grep CONTACTEMAIL= |cut -d= -f 2)
+  echo "OWNER_EMAIL = $OWNER_EMAIL"
+  OWNER_DNS=$(sudo cat /var/cpanel/users/$OWNER |grep DNS= |cut -d= -f 2)
+  echo "OWNER_DNS = $OWNER_DNS"
+fi
+
+echo -e "\nscanning ...\n"
+
+sudo /app/bin/hackscan.sh -q /home/$USERNAME
+
+echo -e "\n\nDone\n"
+
+exit 0
