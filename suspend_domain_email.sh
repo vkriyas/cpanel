071d6325629779b55515a8ea36a410e8ea03f649
riyasvk <riyasvk>
2019-03-05 07:11:58 +0530

    commit

---
+++ suspend_domain_email.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+USER=$1
+TICKET=$2
+
+if [ "$USER" == "" ]; then
+	echo "USER must be specified"
+	exit 1
+fi
+
+if [ "$TICKET" == "" ]; then
+        echo "TICKET must be specified"
+        exit 1
+fi
+
+sudo /app/bin/suspension-backend.sh -u $USER -s addon -l hacked -e "Please refer ticket $TICKET or contact support." -m
+sudo /app/bin/suspension-backend.sh -u $USER -s http -l hacked -e "Please refer ticket # $TICKET or contact support." -m
+sudo /app/bin/suspension-backend.sh -u $USER -s email -l spamming -e "Please refer ticket # $TICKET or contact support." -m
